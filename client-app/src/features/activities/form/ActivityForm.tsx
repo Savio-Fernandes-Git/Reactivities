941fa4f84cb330e52c20d8ef9ebee1aa5b447899
Savio Fernandes <saviof219@gmail.com>
2021-07-21 15:52:42 +0300

    End of Section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props{
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity : Activity) => void;
+    submitting: boolean;
 }
 
-const ActivityForm = ({activity: selectedActivity, closeForm, createOrEdit} : Props) => {
+const ActivityForm = ({activity: selectedActivity, closeForm, createOrEdit, submitting} : Props) => {
     // : helps to rename
     const initialState = selectedActivity ?? {
         id: '',
@@ -41,13 +42,13 @@ const ActivityForm = ({activity: selectedActivity, closeForm, createOrEdit} : Pr
                 value={activity.description} name='description' onChange={handleInputChange}/>
                 <Form.Input placeholder='Category' 
                 value={activity.category} name='category' onChange={handleInputChange}/>
-                <Form.Input placeholder='Date'
+                <Form.Input type='date' placeholder='Date'
                 value={activity.date} name='date' onChange={handleInputChange}/>
                 <Form.Input placeholder='City'
                 value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue'
                 value={activity.venue} name='venue' onChange={handleInputChange}/>
-                <Button floated='right' positive type='submit' content='Submit'/>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>

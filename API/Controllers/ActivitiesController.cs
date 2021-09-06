a98a3d1d796ab70b79806887a253c3b41c6b2951
Savio-Fernandes-Git <saviof219@gmail.com>
2021-09-06 13:14:01 +0300

    End of Section 14

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -29,11 +29,16 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
+        //To only permit host tot edt the activity
+        [Authorize (Policy = "IsActivityHost") ]
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity){
             activity.Id = id;
             return HandleResult(await Mediator.Send(new Edit.Command{Activity = activity}));
         }
+
+        //To only permit host tot edt the activity
+        [Authorize (Policy = "IsActivityHost") ]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id){
             return HandleResult(await Mediator.Send(new Delete.Command{Id=id}));

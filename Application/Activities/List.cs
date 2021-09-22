f01cdd7fc42955c5dceff2c304d56998b04763fc
Savio Fernandes <saviof219@gmail.com>
2021-09-22 21:38:32 +0300

    Adding placeholders

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -16,7 +16,7 @@ namespace Application.Activities
     {
         public class Query : IRequest<Result<PagedList<ActivityDto>>> 
         { 
-            public PagingParams Params { get; set; }
+            public ActivityParams Params { get; set; }
         }//dont need dependencies when were just returning a list
 
         //implement interface using quick fix
@@ -36,9 +36,20 @@ namespace Application.Activities
             public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var query = _context.Activities
+                    .Where(d =>d.Date >= request.Params.StartDate)
                     .OrderBy(d => d.Date)
                     .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new {currentUsername = _userAccessor.GetUsername()})
                     .AsQueryable();
+
+                if (request.Params.IsGoing && !request.Params.IsHost)
+                {
+                    query = query.Where(x => x.Attendees.Any(a => a.Username == _userAccessor.GetUsername()));
+                }
+
+                if(request.Params.IsHost && !request.Params.IsGoing)
+                {
+                    query = query.Where(x => x.HostUsername == _userAccessor.GetUsername());
+                }
 
                 //var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);//source in () destination in <>
 
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,11 +2,11 @@ import { observer } from 'mobx-react-lite';
 import { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
 import { Grid, Loader } from 'semantic-ui-react';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { PagingParams } from '../../../app/models/pagination';
 import { useStore } from '../../../app/stores/store';
 import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
 const ActivityDashboard = () => {
 
@@ -25,19 +25,26 @@ const ActivityDashboard = () => {
         loadActivities();
     }, [activityRegistry.size, loadActivities])
 
-    if(activityStore.loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...' />
+    // if(activityStore.loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...' />
     return (
             <Grid stackable>
                 <Grid.Column width='10'>
                     {/* pagination is an object so we need to cast that into boolean by !! */}
-                    <InfiniteScroll
-                        pageStart={0}
-                        loadMore={handleGetNext}
-                        hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
-                        initialLoad={false}
+                    {activityStore.loadingInitial && !loadingNext ? (
+                        <>
+                            <ActivityListItemPlaceholder />
+                            <ActivityListItemPlaceholder /> 
+                        </>
+                    ) : (
+                        <InfiniteScroll
+                            pageStart={0}
+                            loadMore={handleGetNext}
+                            hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                            initialLoad={false}
                     >
                         <ActivityList />
                     </InfiniteScroll>
+                    )}
                 </Grid.Column>
                 <Grid.Column width='6'>
                     <ActivityFilters />
